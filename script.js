2c0e22d1df6db1972aabd575844645484f5c8ecf
Anurag Singh Procodrr <76770313+procodrr@users.noreply.github.com>
2024-01-24 11:58:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-function addTodo() {
-  var input = document.getElementById('todo-input');
-  var newTodo = input.value.trim();
+const input = document.getElementById('todo-input')
+const todoList = document.getElementById('todo-list')
+
+function addTodo(event) {
+  event.preventDefault()
+  const newTodo = input.value.trim()
 
   if (newTodo) {
-      var li = document.createElement('li');
-      li.textContent = newTodo;
-      li.onclick = function() {
-          this.parentNode.removeChild(this);
-      };
+    const li = document.createElement('li')
+    li.textContent = newTodo
+    todoList.addEventListener('click', (e) => {
+      this.removeChild(e.target)
+    })
 
-      document.getElementById('todo-list').appendChild(li);
-      input.value = '';
+    todoList.appendChild(li)
+    input.value = ''  
   } else {
-      alert('Please enter a task!');
+    console.log('Please enter a task!')
   }
 }
